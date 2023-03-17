97e94dbff0b9e7f8a1c1cf9d003d7d59fe57803d
kaho-lc <lc1536328699@163.com>
2023-03-17 21:29:49 +0800

    初始化顺序表

---
+++ ordertable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define MaxSize 20
+typedef struct
+{
+    int element[MaxSize];
+    int length;
+};
+//初始化顺序表
+void initOrderTable()
+{
+    
+}
+
+int main()
+{
+
+    return 0;
+}
